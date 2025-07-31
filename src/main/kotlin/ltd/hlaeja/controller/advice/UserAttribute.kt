298ce3028c2e06c328163aaba5e6775f8c46c642
Swordsteel <swordsteel@gmail.com>
2025-07-31 09:20:23 +0200

    add default error
    - add error setting in application.yml
    - update tailwind.css
    - add error.html
    - add information.html
    - update UserAttribute with GuestUser
    - add ErrorAttributes
    - add GuestUser

--- src/main/kotlin/ltd/hlaeja/controller/advice/UserAttribute.kt
+++ src/main/kotlin/ltd/hlaeja/controller/advice/UserAttribute.kt
@@ -1,29 +1,24 @@
 package ltd.hlaeja.controller.advice
 
-import java.util.UUID
 import kotlinx.coroutines.reactive.awaitFirstOrNull
 import ltd.hlaeja.security.RemoteAuthentication
-import ltd.hlaeja.security.RemoteUserDetail
+import ltd.hlaeja.security.user.GuestUser
 import org.springframework.security.core.context.ReactiveSecurityContextHolder
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ModelAttribute
 
 @ControllerAdvice
-class UserAttribute {
+class UserAttribute : GuestUser {
 
     @ModelAttribute
     suspend fun remoteUser(model: Model) {
-        val remoteAuthentication: RemoteAuthentication = ReactiveSecurityContextHolder.getContext()
-            .awaitFirstOrNull()
-            ?.let { it.authentication as RemoteAuthentication }
-            ?: RemoteAuthentication(
-                RemoteUserDetail(
-                    UUID.fromString("00000000-0000-0000-0000-000000000000"),
-                    "n/a",
-                ),
-                mutableListOf(),
-            )
-        model.addAttribute("remoteUser", remoteAuthentication)
+        model.addAttribute(
+            "remoteUser",
+            ReactiveSecurityContextHolder.getContext()
+                .awaitFirstOrNull()
+                ?.let { it.authentication as RemoteAuthentication }
+                ?: guestUser(),
+        )
     }
 }
