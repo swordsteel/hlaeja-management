a1d0501feea1ecca40d6d696cc852702052cee5e
Swordsteel <swordsteel@gmail.com>
2025-07-31 23:04:36 +0200

    extract paths from SecurityConfiguration to authorize

---
+++ src/main/kotlin/ltd/hlaeja/security/authorize/AdminPaths.kt
@@ -0,0 +1,8 @@
+package ltd.hlaeja.security.authorize
+
+import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec
+
+fun AuthorizeExchangeSpec.adminPaths(): AuthorizeExchangeSpec.Access = pathMatchers(
+    "/account/**",
+    "/type/**",
+)
