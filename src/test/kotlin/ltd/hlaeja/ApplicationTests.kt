57565b4589fa07799153ca430d308cdd383825c5
Swordsteel <swordsteel@gmail.com>
2025-08-06 15:07:21 +0200

    disable spring boot test

--- src/test/kotlin/ltd/hlaeja/ApplicationTests.kt
+++ src/test/kotlin/ltd/hlaeja/ApplicationTests.kt
@@ -1,9 +1,8 @@
 package ltd.hlaeja
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+// @SpringBootTest
 class ApplicationTests {
 
     @Test
